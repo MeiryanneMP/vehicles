fdfe9ff56e208bc94993e1d792ba1bda1f67c3df
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2025-12-09 18:24:58 -0300

    feat: adiciona configuração para inserir imagens

--- vehicles/models.py
+++ vehicles/models.py
@@ -16,7 +16,9 @@ class Vehicle(models.Model):
         Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='vehicles', blank=True, null=True)
 
     def __str__(self):
         return self.model
